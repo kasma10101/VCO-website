05d32f49ddc767e43e45208103473a9b6675fe97
adnan4k <fayomuhe5@gmail.com>
2024-03-02 22:34:50 +0300

    default changed

--- model/user.js
+++ model/user.js
@@ -7,7 +7,7 @@ const userSchema = new Schema({
     email: {required: true, type: String},
     password: {required: true, type: String, minLength: 8},
     createdAt: {default: Date.now,  type: Date},
-    isAdmin: {default: true, type: Boolean},
+    isAdmin: {default: false, type: Boolean},
     donationPlan: {type: String}
 })
 
